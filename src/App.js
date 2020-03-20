a17844e850902f1692d1a57d74baff085ad7ef08
A Groshek <amy@amygroshek.com>
2020-03-20 13:37:25 -0500

    Get basic example up and working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL, {Source, Layer} from 'react-map-gl';
+import {json as requestJson} from 'd3-request';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import {dataLayer} from './components/atoms/map-style.js';
+import {updatePercentiles} from './utils/utils';
+import ControlPanel from './components/molecules/control-panel';
+
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN; // Set your mapbox token here
+
+export default class App extends Component {
+  state = {
+    year: 2015,
+    data: null,
+    hoveredFeature: null,
+    viewport: {
+      latitude: 40,
+      longitude: -100,
+      zoom: 3,
+      bearing: 0,
+      pitch: 0
+    }
+  };
+
+  componentDidMount() {
+    requestJson(
+      'https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson',
+      (error, response) => {
+        if (!error) {
+          this._loadData(response);
+        }
+      }
+    );
+  }
+
+  _loadData = data => {
+    this.setState({
+      data: updatePercentiles(data, f => f.properties.income[this.state.year])
+    });
+  };
+
+  _updateSettings = (name, value) => {
+    if (name === 'year') {
+      this.setState({year: value});
+
+      const {data} = this.state;
+      if (data) {
+        // trigger update
+        this.setState({
+          data: updatePercentiles(data, f => f.properties.income[value])
+        });
+      }
+    }
+  };
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+  _onHover = event => {
+    const {
+      features,
+      srcEvent: {offsetX, offsetY}
+    } = event;
+    const hoveredFeature = features && features.find(f => f.layer.id === 'data');
+
+    this.setState({hoveredFeature, x: offsetX, y: offsetY});
+  };
+
+  _renderTooltip() {
+    const {hoveredFeature, x, y} = this.state;
+
+    return (
+      hoveredFeature && (
+        <div className="tooltip" style={{left: x, top: y}}>
+          <div>State: {hoveredFeature.properties.name}</div>
+          <div>Median Household Income: {hoveredFeature.properties.value}</div>
+          <div>Percentile: {(hoveredFeature.properties.percentile / 8) * 100}</div>
+        </div>
+      )
+    );
+  }
+
+  render() {
+    const {viewport, data} = this.state;
+    console.log(data);
+
+    return (
+      <div style={{height: '100%', position: 'relative'}}>
+        <MapGL
+          {...viewport}
+          width="100%"
+          height="600px"
+          mapStyle="mapbox://styles/mapbox/light-v9"
+          onViewportChange={this._onViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onHover={this._onHover}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Source type="geojson" data={data}>
+            <Layer {...dataLayer} />
+          </Source>
+          {this._renderTooltip()}
+        </MapGL>
+
+        <ControlPanel
+          containerComponent={this.props.containerComponent}
+          settings={this.state}
+          onChange={this._updateSettings}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export function renderToDom(container) {
+  render(<App />, container);
+}
